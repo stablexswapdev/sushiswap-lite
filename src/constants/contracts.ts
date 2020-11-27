404f9d1e435f4980f8d4de7735dc3fcc46bf161f
stablexswapdev <73826792+stablexswapdev@users.noreply.github.com>
2020-11-27 20:07:24 +0800

    Update contracts.ts

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -3,10 +3,12 @@ import Settlement from "@sushiswap/settlement/deployments/mainnet/Settlement.jso
 
 // export const UNISWAP_ROUTER = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 export const SUSHISWAP_ROUTER = "0xd9e1ce17f2641f24ae83637ab66a2cca9c378b9f";
+
+export const STABLEXSWAP_ROUTER = "0xc7fa7951187f91153851b0f9ab2d032abd0f2f0c";
 export const ROUTER = SUSHISWAP_ROUTER;
-export const MASTER_CHEF = "0xc2edad668740f1aa35e4d8f227fb8e17dca888cd";
-export const SUSHI_BAR = "0x8798249c2e607446efb7ad49ec89dd1865ff4272";
-export const SUSHI_ROLL = "0x16E58463eb9792Bc236d8860F5BC69A81E26E32B";
+// export const MASTER_CHEF = "0xc2edad668740f1aa35e4d8f227fb8e17dca888cd";
+// export const SUSHI_BAR = "0x8798249c2e607446efb7ad49ec89dd1865ff4272";
+// export const SUSHI_ROLL = "0x16E58463eb9792Bc236d8860F5BC69A81E26E32B";
 export const LP_TOKEN_SCANNER = "0xD132Ce8eA8865348Ac25E416d95ab1Ba84D216AF";
 export const ORDER_BOOK = OrderBook.address;
 export const SETTLEMENT = Settlement.address;
